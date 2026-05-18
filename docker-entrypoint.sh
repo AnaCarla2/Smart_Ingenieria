271a12af6b69ec3284b570ac5932bfc5614988ad
Anacarla2 <anacarlamartinezcaraballo@gmail.com>
2026-05-18 14:57:06 -0500

    Debug: mostrar logs de Laravel

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,5 +7,9 @@ php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
 
+# Mostrar errores de Laravel en logs
+php artisan about --no-ansi || true
+cat storage/logs/laravel.log 2>/dev/null || true
+
 # Arrancar Apache
 exec "$@"
